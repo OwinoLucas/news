d1e890925d2cfdd4bc5d968a2cbcd06e5c53c0a6
Lucas Owino <lucowish35@gmail.com>
2020-04-26 01:21:08 +0300

    added the code that fetched news from 4 diff sources

--- app/views.py
+++ app/views.py
@@ -12,8 +12,12 @@ def index():
     #getting news_source category
     abc_news = get_news_source('abc-news')
     cnn_news = get_news_source('cnn')
+    time_news = get_news_source('time')
+    nbc_news = get_news_source('nbc-news')
+    fox_news = get_news_source('fox-news')
+    the_wall_street_journal = get_news_source('the-wall-street-journal')
     title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title = title, abc = abc_news, cnn = cnn_news)
+    return render_template('index.html', title = title, abc = abc_news, cnn = cnn_news, time = time_news , nbc = nbc_news, fox = fox_news, wallstreet = the_wall_street_journal)
 
 @app.route('/news_source/<news_source_id>')
 def news_source(news_id):
