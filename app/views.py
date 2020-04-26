adee670b199bf7baff4641f7790e97f4451440ef
Lucas Owino <lucowish35@gmail.com>
2020-04-26 15:30:07 +0300

    replaced the base.html content with {% extends 'bootstrap/base.html'%} as bootstrap comes with its own base.html

--- app/views.py
+++ app/views.py
@@ -15,9 +15,10 @@ def index():
     time_news = get_news_source('time')
     nbc_news = get_news_source('nbc-news')
     fox_news = get_news_source('fox-news')
+    espn_news = get_news_source('espn')
     the_wall_street_journal = get_news_source('the-wall-street-journal')
     title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title = title, abc = abc_news, cnn = cnn_news, time = time_news , nbc = nbc_news, fox = fox_news, wallstreet = the_wall_street_journal)
+    return render_template('index.html', title = title, abc = abc_news, cnn = cnn_news, time = time_news , nbc = nbc_news, fox = fox_news, wallstreet = the_wall_street_journal, espn = espn_news)
 
 @app.route('/news_source/<news_source_id>')
 def news_source(news_id):
