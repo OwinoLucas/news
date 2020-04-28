11d8ae2c206f5139600c88e8911d539dbcfe5982
Lucas Owino <lucowish35@gmail.com>
2020-04-28 15:00:22 +0300

    deyployment to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-NEWS_API_KEY = '6467f60f55ef47f4a6348aa5e9f91ccf'
+NEWS_API_KEY='6467f60f55ef47f4a6348aa5e9f91ccf'
 #SECRET_KEY = '\xc9\xe1\x17\xe0\xf6\x7f\x00\t\x9f8\x8cJg\xdeLwG\xaf\xbe\x1f\xb8\n\x04\x00'
 
-FLASK_APP=manager.py flask run
+#FLASK_APP=manager.py flask run
 
-#python3.6 manager.py server
+python3.6 manager.py server
